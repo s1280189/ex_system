850297300c332685ac6367825cb13c945e6e9423
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2023-01-11 12:31:31 +0900

    Create c_1d_dft.c

---
+++ ex05/part1/c_1d_dft.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<complex.h>
+#include<math.h>
+
+#define MAX 1024
+float complex x[MAX*MAX], y[MAX*MAX], z[MAX*MAX];
+
+void dft_1d(float complex a[], float complex res[], int length){
+  //write code here  
+}
+
+
+void idft_1d(float complex a[], float complex res[], int length){
+  //write code here
+}
+
+
+int main(){
+  int n,i;
+
+  scanf("%d",&n);
+  for(i=0;i<n;i++){
+    float tmp;
+    scanf("%f", &tmp);
+    x[i]=temp;
+  }
+  dft_1d(x, y , n);
+  idft_1d(y, z, n);
+
+  for(i=0;i<n;i++){
+    printf("%.3f%+.3fi\n", crealf(y[i]), cimagf(y[i]));
+  }
+
+
+  for(i=0;i<n;i++){
+     printf("%.3f%+.3fi\n", crealf(z[i]), cimagf(z[i]));
+  }
+
+  for(i=0;i<n;i++){
+    printf("%.3f\n", cabsf(z[i]));
+  }
+
+  return 0;
+}
